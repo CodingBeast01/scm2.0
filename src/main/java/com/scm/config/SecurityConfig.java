c10abd35a6f822b8d2d84e3646f79f1472bba1d8
Kritiman Singh <kritimansingh123@gmail.com>
2025-08-07 18:01:25 +0530

    adding Security

---
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.scm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+
+@Configuration
+public class SecurityConfig {
+
+
+    // user create and login using java code with in memory service
+
+    // @Bean
+    // public UserDetailsService userDetailsService() {
+
+    // UserDetails user1 = User
+    // .withDefaultPasswordEncoder()
+    // .username("admin123")
+    // .password("admin123")
+    // .roles("ADMIN", "USER")
+    // .build();
+
+    // UserDetails user2 = User
+    // .withDefaultPasswordEncoder()
+    // .username("user123")
+    // .password("password")
+    // // .roles(null)
+    // .build();
+
+    public AuthenticationProvider
+
+
+
+}
