5914a3bde55cae2c9707fb2be181674e195c8d62
Kritiman Singh <kritimansingh123@gmail.com>
2025-08-08 22:44:51 +0530

    adding security

--- src/main/java/com/scm/helpers/AppConstants.java
+++ src/main/java/com/scm/helpers/AppConstants.java
@@ -3,7 +3,7 @@ package com.scm.helpers;
 public class AppConstants {
 
 
-//public static final String APP_NAME = "SCM Application";
+public static final String APP_NAME = "SCM Application";
     public static final String ROLE_USER = "ROLE_USER";
 
 
