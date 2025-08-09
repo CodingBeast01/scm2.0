7cd21b2d3ea543aafb5041c78c2129b1d6eb95ca
Kritiman Singh <kritimansingh123@gmail.com>
2025-08-09 17:17:00 +0530

    adding Oauth2

--- src/main/java/com/scm/controllers/UserController.java
+++ src/main/java/com/scm/controllers/UserController.java
@@ -1,9 +1,12 @@
 package com.scm.controllers;
 
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.scm.helpers.Helper;
 
 
 @Controller
@@ -16,7 +19,9 @@ public class UserController {
     }
 
     @RequestMapping(value ="/profile", method=RequestMethod.GET)
-    public String userProfile() {
+    public String userProfile(Authentication authentication) {
+       // String email = Helper.getEmailOfLoggedInUser(authentication);
+        // Use the email as needed
         return "user/profile";
     }
     
