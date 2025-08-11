c3d44eaa548ea57df0d8fdd7884d59fbac0d3ba1
Kritiman Singh <kritimansingh123@gmail.com>
2025-08-11 18:13:12 +0530

    added form handler

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -1,15 +1,20 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.scm.forms.ContactForm;
 
 @Controller
 @RequestMapping("/user/contact")
 public class ContactController {
 
     @RequestMapping("/add")
-    public String addContactView() {
-
+    public String addContactView(Model model) {
+        ContactForm contactForm = new ContactForm();
+        contactForm.setName("Raj Singh");
+        model.addAttribute("contactForm", contactForm);
         return "user/add_Contact";
     }
 
