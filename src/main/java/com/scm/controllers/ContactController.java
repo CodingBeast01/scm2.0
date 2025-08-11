921442b479a1c608af1eb354900f576e452f2111
Kritiman Singh <kritimansingh123@gmail.com>
2025-08-11 17:59:40 +0530

    adding Contact controller

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -1,5 +1,16 @@
 package com.scm.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/contact")
 public class ContactController {
+
+    @RequestMapping("/add")
+    public String addContactView() {
+
+        return "user/add_Contact";
+    }
 
 }
