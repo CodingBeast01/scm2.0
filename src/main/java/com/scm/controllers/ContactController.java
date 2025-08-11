31c307010c84c7100db8982210493f4ea4b97c55
Kritiman Singh <kritimansingh123@gmail.com>
2025-08-11 18:47:45 +0530

    adding validation

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -1,18 +1,29 @@
 package com.scm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.scm.entities.Contact;
+import com.scm.entities.User;
 import com.scm.forms.ContactForm;
+import com.scm.helpers.Helper;
+import com.scm.services.ContactService;
+import com.scm.services.UserService;
 
 @Controller
 @RequestMapping("/user/contact")
 public class ContactController {
 
-    
+    @Autowired
+    ContactService contactService;
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("/add")
     public String addContactView(Model model) {
@@ -23,8 +34,26 @@ public class ContactController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String saveContact(@ModelAttribute ContactForm contactForm) {
+    public String saveContact(@ModelAttribute ContactForm contactForm , Authentication authentication) {
         System.out.println("Contact saved: " + contactForm);
+
+        String username = Helper.getEmailOfLoggedInUser(authentication);
+
+         User user = userService.getUserByEmail(username);
+        //form -- contact
+        Contact contact = new Contact();
+          contact.setName(contactForm.getName());
+        contact.setFavorite(contactForm.isFavorite());
+        contact.setEmail(contactForm.getEmail());
+        contact.setPhoneNumber(contactForm.getPhoneNumber());
+        contact.setAddress(contactForm.getAddress());
+        contact.setDescription(contactForm.getDescription());
+        contact.setUser(user);
+        contact.setLinkedInLink(contactForm.getLinkedInLink());
+        contact.setWebsiteLink(contactForm.getWebsiteLink());
+
+
+        contactService.save(contact );
         return "redirect:/user/contacts/add";
     }
 
---
+++ src/main/java/com/scm/services/impl/ContactServicesImpl.java
@@ -0,0 +1,84 @@
+package com.scm.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import com.scm.entities.Contact;
+import com.scm.entities.User;
+import com.scm.repsitories.ContactRepo;
+import com.scm.services.ContactService;
+
+@Service
+public class ContactServicesImpl implements ContactService {
+
+
+    @Autowired
+    private ContactRepo contactRepo;
+
+
+    @Override
+    public Contact save(Contact contact) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public Contact update(Contact contact) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public List<Contact> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public Contact getById(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
+
+    @Override
+    public void delete(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public Page<Contact> searchByName(String nameKeyword, int size, int page, String sortBy, String order, User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchByName'");
+    }
+
+    @Override
+    public Page<Contact> searchByEmail(String emailKeyword, int size, int page, String sortBy, String order,
+            User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchByEmail'");
+    }
+
+    @Override
+    public Page<Contact> searchByPhoneNumber(String phoneNumberKeyword, int size, int page, String sortBy, String order,
+            User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchByPhoneNumber'");
+    }
+
+    @Override
+    public List<Contact> getByUserId(String userId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getByUserId'");
+    }
+
+    @Override
+    public Page<Contact> getByUser(User user, int page, int size, String sortField, String sortDirection) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getByUser'");
+    }
+
+}
