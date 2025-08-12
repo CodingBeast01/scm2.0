7b6a73196c05273079f902905b56ca032aad6c7f
Kritiman Singh <kritimansingh123@gmail.com>
2025-08-12 19:52:34 +0530

    feat: add search functionality for contacts with pagination and sorting

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -1,6 +1,7 @@
 package com.scm.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -125,4 +126,46 @@ public class ContactController {
 
         return "user/contacts";
     }
+
+    //search handler
+
+    @RequestMapping("/search")
+    public String searchHandler(
+
+            @ModelAttribute ContactSearchForm contactSearchForm,
+            @RequestParam(value = "size", defaultValue = AppConstants.PAGE_SIZE + "") int size,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "sortBy", defaultValue = "name") String sortBy,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction,
+            Model model,
+            Authentication authentication) {
+
+        logger.info("field {} keyword {}", contactSearchForm.getField(), contactSearchForm.getValue());
+
+        var user = userService.getUserByEmail(Helper.getEmailOfLoggedInUser(authentication));
+
+        Page<Contact> pageContact = null;
+        if (contactSearchForm.getField().equalsIgnoreCase("name")) {
+            pageContact = contactService.searchByName(contactSearchForm.getValue(), size, page, sortBy, direction,
+                    user);
+        } else if (contactSearchForm.getField().equalsIgnoreCase("email")) {
+            pageContact = contactService.searchByEmail(contactSearchForm.getValue(), size, page, sortBy, direction,
+                    user);
+        } else if (contactSearchForm.getField().equalsIgnoreCase("phone")) {
+            pageContact = contactService.searchByPhoneNumber(contactSearchForm.getValue(), size, page, sortBy,
+                    direction, user);
+        }
+
+        logger.info("pageContact {}", pageContact);
+
+        model.addAttribute("contactSearchForm", contactSearchForm);
+
+        model.addAttribute("pageContact", pageContact);
+
+        model.addAttribute("pageSize", AppConstants.PAGE_SIZE);
+
+        return "user/search";
+    }
+
+
 }
--- src/main/java/com/scm/helpers/AppConstants.java
+++ src/main/java/com/scm/helpers/AppConstants.java
@@ -12,6 +12,8 @@ public static final int CONTACT_IMAGE_WIDTH = 500;
 public static final int CONTACT_IMAGE_HEIGHT = 500;
 public static final String CONTACT_IMAGE_CROP = "fill";
 
+public static final String PAGE_SIZE = "10";
+
 
 }
     
