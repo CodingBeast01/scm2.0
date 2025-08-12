0b68fcebeb374eb358b08934d0fcc35f4346110c
Kritiman Singh <kritimansingh123@gmail.com>
2025-08-12 17:32:49 +0530

    refactor AppConfig to use properties for Cloudinary configuration; add application-dev.properties file

--- src/main/java/com/scm/config/AppConfig.java
+++ src/main/java/com/scm/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.scm.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +10,19 @@ import com.cloudinary.utils.ObjectUtils;
 @Configuration
 public class AppConfig {
 
+    @Value("${cloudinary.cloud.name}")
+    private String cloudName;
+    @Value("${cloudinary.api.key}")
+    private String apiKey;
+    @Value("${cloudinary.api.secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "your_cloud_name",
-                "api_key", "your_api_key",
-                "api_secret", "your_api_secret"
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
         ));
     }
 
