f601804066c5ba02e4bfd03af47426ecef416afd
Kritiman Singh <kritimansingh123@gmail.com>
2025-08-12 17:51:43 +0530

    addint contact controller

--- src/main/java/com/scm/entities/Contact.java
+++ src/main/java/com/scm/entities/Contact.java
@@ -37,6 +37,8 @@ public class Contact {
     private String websiteLink;
     private String linkedInLink;
    // private List<String> socialLinks = new ArrayList<>();
+
+   private String cloudinaryImagePublicID
     
    @ManyToOne
    private User user;
